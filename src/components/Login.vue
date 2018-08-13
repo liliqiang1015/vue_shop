75cac368ca4ff0acd9af6d2bfd7832c106b6ddef
Leki <535995777@qq.com>
2018-08-13 15:19:49 +0800

    完成登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="login-container">
+
+        <div class="login-box">
+            <div class="avatar-box">
+                <img src="../assets/images/logo.png">
+            </div>
+
+            <!-- 登录表单 -->
+            <!-- :model 绑定是表单的数据对象 -->
+            <!-- :rules 是表单的验证规则对象 -->
+            <!-- ref 是当亲 表单组件的引用对象 -->
+            <el-form :model="loginForm" :rules="loginFormRules" ref="loginFormRef">
+                <!-- el-form-item 的 prop属性， 专门用来指定校验规则 所有的校验规则 都在 :rules 所绑定的对象中定义 -->
+                <el-form-item prop="username">
+                    <!-- el-input 是一个文本框 使用v-model指令 把文本框的值 双向绑定 -->
+                    <el-input v-model="loginForm.username">
+                        <i slot="prefix" class="iconfont icon-user"></i>
+                    </el-input>
+                </el-form-item>
+
+                <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" type="password">
+                        <i slot="prefix" class="iconfont icon-3702mima"></i>
+                    </el-input>
+                </el-form-item>
+
+                <el-row>
+                    <el-col :offset="15">
+                        <el-button type="primary" @click="login">登录</el-button>
+                        <el-button type="warning" @click="resetForm">重置</el-button>
+                    </el-col>
+                </el-row>
+
+            </el-form>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetForm () {
+      // 调用 resetFields 方法重置表单
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      // 调用validate 进行表单的预验证、
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        // 验证通过
+        const {data: res} = await this.$http.post('login',this.loginForm)
+        if (res.meta.status !== 200) {
+          window.sessionStorage.removeItem('token')
+          return this.$message.error('登录失败')
+        }this.$message.console.error('登录失败')
+        this.$message.success('登录成功!')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  overflow: hidden;
+}
+.login-box {
+  width: 450px;
+  height: 304px;
+  background: #fff;
+  border-radius: 4px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.avatar-box {
+  width: 130px;
+  height: 130px;
+  border-radius: 50%;
+  border: 1px solid #eee;
+  padding: 8px;
+  box-shadow: 0 0 10px #eee;
+  position: absolute;
+  top: -65px;
+  background: #fff;
+  left: 50%;
+  transform: translateX(-50%);
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: #eee;
+  }
+}
+
+.el-form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+// 配置axios
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
